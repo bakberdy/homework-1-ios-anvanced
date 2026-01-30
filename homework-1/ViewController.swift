d42d38ef06c8ce30c7b666e09cf50ade21c328b5
Bakberdi <bakberdy.ye@gmail.com>
2026-01-30 14:16:24 +0500

    Initial Commit

---
+++ homework-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework-1
+//
+//  Created by Bakberdi Esentai on 30.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
